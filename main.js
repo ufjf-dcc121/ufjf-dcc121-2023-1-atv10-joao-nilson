0c1ecb34bb3adab756c1194efe03dcb22b778872
joao <jnilson@ice.ufjf.br>
2023-05-30 19:27:48 -0300

    adiciona um estado basico

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const form = document.forms.entrada;
 form.addEventListener('submit', envia);
 
+let estado = 0;
+
 function envia(evento) {
     evento.preventDefault();
     console.log('Formulario enviado!');
+    estado++;
+    atualiza();
+}
+
+function atualiza(){
+    const ol = document.querySelector('ol');
+    ol.innerHTML = `<li>${estado}</li>`;
 }

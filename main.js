4e070d7ceb99fb366d9f3d8fce3e8a67fd342e53
joao <jnilson@ice.ufjf.br>
2023-05-30 20:40:45 -0300

    isola o estado

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import store from "./store.js";
+import {adicionar, getItems} from "./store.js";
 
 const form = document.forms.entrada;
 form.addEventListener('submit', envia);
@@ -9,7 +9,7 @@ function envia(evento) {
     evento.preventDefault();
     console.log('Formulario enviado!');
     const n = form.valor.value;
-    store.estado.push(n);
+    adicionar(n);
     form.valor.value = "";
     form.valor.focus();
     atualiza();
@@ -18,10 +18,11 @@ function envia(evento) {
 function atualiza(){
     const ol = document.querySelector('ol');
     ol.innerHTML = "";
-    for(let i = 0; i < store.estado.length; i++)
+    for(let i = 0; i < getItems().length; i++)
     {
         const li = document.createElement('li');
-        li.textContent = store.estado[i];
+        li.textContent = getItems()[i];
         ol.appendChild(li);
     }
+    getItems().push("Boom");
 }
